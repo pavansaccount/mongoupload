6d2ec75d748b5c73a4e1082b4d7a86a1067aded7
pavankalyan <36667911+pavankalyanreddy5@users.noreply.github.com>
2018-12-17 19:38:01 +0530

    upload

--- server.js
+++ server.js
@@ -152,6 +152,6 @@ app.delete('/files/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 8080;
+const port = 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
